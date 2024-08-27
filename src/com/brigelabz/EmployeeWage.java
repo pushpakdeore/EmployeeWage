c4876042058984fb34e87b6c28a95ab0fc217129
pushpakdeore <deorepushpakk@gmail.com>
2024-08-27 22:51:38 +0530

    Added: Part-Time funtion to code

--- src/com/brigelabz/EmployeeWage.java
+++ src/com/brigelabz/EmployeeWage.java
@@ -1,39 +1,59 @@
 package com.brigelabz;
 import java.util.*;
-class EmployeeWage{
+public class EmployeeWage{
+    public final int IS_PART_TIME = 1;
+    public final int IS_FULL_TIME = 2;
 
-    private int wagePerHr=20;
-    private int FullDayHr=8;
-    private int totalWage=0;
+    public void calculateEmpWage() {
+        int empHrs;
+        int totalEmpHrs = 0;
+        int totalEmpWage=0;
+        int totalWorkingDays=0;
+        int wagePerHrs=20;
+        int numOfWorkingDays=20;
+        int numOfWorkingHrs=160;
 
-    public void dailywage(boolean add){
+        while (totalEmpHrs <= numOfWorkingHrs && totalWorkingDays <= numOfWorkingDays) {
+            Random random = new Random();
+            // Generate a random 0 ,1 or 2
+            int empCheck = random.nextInt(3);
 
-        if(add==true){
-
-            totalWage=totalWage+(wagePerHr *FullDayHr);
-        }else{
-
-            totalWage=totalWage+0;
+            switch ((int) empCheck) {
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present as PART TIME");
+                    empHrs = 4;
+                    totalEmpHrs += empHrs;
+                    totalEmpWage+=empHrs*wagePerHrs;
+                    System.out.println("Total Wage :"+totalEmpWage);
+                    break;
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present as FULL TIME");
+                    empHrs = 8;
+                    totalEmpHrs += empHrs;
+                    totalEmpWage+=empHrs*wagePerHrs;
+                    System.out.println("Total Wage :"+totalEmpWage);
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+                    totalEmpHrs += empHrs;
+                    totalEmpWage+=empHrs*wagePerHrs;
+                    System.out.println("Total Wage :"+totalEmpWage);
+                    break;
+            }
+            totalWorkingDays++;
+            System.out.println("Day No :" + totalWorkingDays + " Emp Hrs: " + empHrs);
         }
-        System.out.println("TotalWage= "+totalWage);
+        totalEmpWage = totalEmpHrs * wagePerHrs;
+        System.out.println("Total Working Hrs for  :"+totalEmpHrs);
     }
-    public void isPresent(){
-        Random random = new Random();
-        int attendance = random.nextInt(2);
 
-        if (attendance == 1) {
-            dailywage(true);
-        }else{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWage ew1 = new EmployeeWage();
+        EmployeeWage ew2 = new EmployeeWage();
 
-            dailywage(false);
-        }
-
-    }
-    public static void main(String[] args){
-
-        System.out.println("Welcome to Employee wage computation program");
-
-        EmployeeWage ew1=new EmployeeWage();
-        ew1.isPresent();
+        ew1.calculateEmpWage();
+        ew2.calculateEmpWage();
     }
 }
