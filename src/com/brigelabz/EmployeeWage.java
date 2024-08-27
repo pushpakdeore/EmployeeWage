5e270cceee329e5eccb4429df0697547ae2a06fb
pushpakdeore <deorepushpakk@gmail.com>
2024-08-27 22:27:13 +0530

    Added:Present or Absent Fuction to code

--- src/com/brigelabz/EmployeeWage.java
+++ src/com/brigelabz/EmployeeWage.java
@@ -1,5 +1,22 @@
 package com.brigelabz;
-
+import java.util.*;
 public class EmployeeWage {
+    public void isPresent(){
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+
+        if (attendance == 0) {
+            System.out.println("Employee is absent.");
+        } else {
+            System.out.println("Employee is present.");
+        }
+    }
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Employee wage computation program");
+
+        EmployeeWage ew1=new EmployeeWage();
+        ew1.isPresent();
+    }
 
 }
