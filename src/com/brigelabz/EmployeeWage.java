7527f03fa1a422cc3280eb179a319af625aed1e0
pushpakdeore <deorepushpakk@gmail.com>
2024-08-27 22:42:54 +0530

    Added:Calculate Daily Employee Wage to Code

--- src/com/brigelabz/EmployeeWage.java
+++ src/com/brigelabz/EmployeeWage.java
@@ -1,15 +1,33 @@
 package com.brigelabz;
 import java.util.*;
-public class EmployeeWage {
+class EmployeeWage{
+
+    private int wagePerHr=20;
+    private int FullDayHr=8;
+    private int totalWage=0;
+
+    public void dailywage(boolean add){
+
+        if(add==true){
+
+            totalWage=totalWage+(wagePerHr *FullDayHr);
+        }else{
+
+            totalWage=totalWage+0;
+        }
+        System.out.println("TotalWage= "+totalWage);
+    }
     public void isPresent(){
         Random random = new Random();
         int attendance = random.nextInt(2);
 
-        if (attendance == 0) {
-            System.out.println("Employee is absent.");
-        } else {
-            System.out.println("Employee is present.");
+        if (attendance == 1) {
+            dailywage(true);
+        }else{
+
+            dailywage(false);
         }
+
     }
     public static void main(String[] args){
 
@@ -18,5 +36,4 @@ public class EmployeeWage {
         EmployeeWage ew1=new EmployeeWage();
         ew1.isPresent();
     }
-
 }
